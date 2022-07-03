3dc08b7bfef938767e33789ee51d78e84d0c5034
Mathieu <923463-mathbou@users.noreply.gitlab.com>
2022-07-03 21:40:55 +0200

    🩹 proper exit code

--- tools/redscript_lint.ps1
+++ tools/redscript_lint.ps1
@@ -6,4 +6,21 @@ $FinalRedscript = "$GameLocation\r6\cache\final.redscripts.bk"
 
 $Src = "$ProjectRoot\src\redscript"
 
-. $RedscriptCli lint -s $Src -b $FinalRedscript
+
+$pinfo = New-Object System.Diagnostics.ProcessStartInfo
+$pinfo.FileName = $RedscriptCli
+$pinfo.RedirectStandardError = $true
+$pinfo.RedirectStandardOutput = $true
+$pinfo.UseShellExecute = $false
+$pinfo.Arguments = "lint -s $Src -b $FinalRedscript"
+$p = New-Object System.Diagnostics.Process
+$p.StartInfo = $pinfo
+$p.Start() | Out-Null
+$p.WaitForExit()
+
+$stdout = $p.StandardOutput.ReadToEnd()
+Write-Host $stdout
+if ($stdout.Contains("ERROR")){
+    exit 1
+}
+exit 0
