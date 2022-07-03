164e4e4590a9930120517d2a098be48f3a2a142b
Mathieu <923463-mathbou@users.noreply.gitlab.com>
2022-07-03 23:42:04 +0200

    🐛 force folder creation

--- tools/2_release.ps1
+++ tools/2_release.ps1
@@ -1,13 +1,13 @@
 $ProjectRoot = (Get-Item $PSScriptRoot).Parent.FullName
 
-New-Item -Path "$ProjectRoot\build\r6\scripts" -ItemType "directory"
+New-Item -Path "$ProjectRoot\build\r6\scripts" -ItemType "directory" -Force
 Copy-Item -Path "$ProjectRoot\src\redscript\*" -Destination "$ProjectRoot\build\r6\scripts" -Force -Recurse
 
 Copy-Item -Path "$ProjectRoot\resources\packed\*" -Destination "$ProjectRoot\build" -Force -Recurse
 
-New-Item -Path "$ProjectRoot\build\red4ext\plugins\in_world_navigation" -ItemType "directory"
+New-Item -Path "$ProjectRoot\build\red4ext\plugins\in_world_navigation" -ItemType "directory" -Force
 Copy-Item -Path "$ProjectRoot\src\red4ext\build\release\bin\*" -Destination "$ProjectRoot\build\red4ext\plugins\in_world_navigation" -Force
 
 # Dist
-New-Item -Path "$ProjectRoot\dist" -Type "Directory"
+New-Item -Path "$ProjectRoot\dist" -Type "Directory" -Force
 Compress-Archive -Path "$ProjectRoot\build\*" -DestinationPath "$ProjectRoot\dist\in_world_navigation.zip" -Force
